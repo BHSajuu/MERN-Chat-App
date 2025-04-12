b99098423072585bb45ab1ce9869eb8da9c34e48
Boktiar Hussain <boktiarh_ug_22@mech.nits.ac.in>
2025-04-13 00:51:02 +0530

    13-04-2025

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,17 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json()); // this will extract the body portion of an incoming request and parse it into a JSON object
+app.use(cookieParser()); // this will parse the cookies in the incoming request and make them available in req.cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
